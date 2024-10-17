68c0ffc1917014a326abe7664d169c0d4ac4ea5b
Ufuk Altin <ufuk.altin@kiongroup.com>
2024-10-17 18:20:40 +0200

    Spring App Commit

---
+++ grade-master-service/src/test/java/de/hsaalen/grademaster/grademasterservice/GradeMasterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hsaalen.grademaster.grademasterservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeMasterServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
